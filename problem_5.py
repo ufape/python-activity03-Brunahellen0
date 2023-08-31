279b33a367fe3f0a91f94dce8dcb4736142e61e2
Brunahellen0 <142901267+Brunahellen0@users.noreply.github.com>
2023-08-31 10:45:43 -0300

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -30,8 +30,82 @@ A sequência é: 1 2 3
 
 
 def main():
-    pass  # your code here
+    # -*- coding: utf-8 -*-
+
+# YOUR FULL NAME
+# UAG00098
+# Problem Set 3 - Problem 4
+# Description:
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+O arquivo de entrada contém vários números inteiros. O último número no arquivo de entrada é 0.
+Exemplo:
+Digite o inteiro (0 - sair): 5
+Digite o inteiro (0 - sair): 10
+Digite o inteiro (0 - sair): 3
+Digite o inteiro (0 - sair): 0
+
+
+Processes:
+Este programa deve ler uma variável inteira X inúmeras vezes (deve parar quando o valor no arquivo de entrada for igual a zero). Para cada valor lido imprima a sequência de 1 até X, com um espaço entre cada número e seu sucessor.
+
+Output(s):
+Para cada número N do arquivo de entrada deve ser impressa uma linha de 1 até N, conforme o exemplo abaixo. Não deve haver espaço em branco após o último valor da linha.
+Exemplo:
+A sequência é: 1 2 3 4 5
+A sequência é: 1 2 3 4 5 6 7 8 9 10
+A sequência é: 1 2 3
+"""
+
+
+def main():
+    # -*- coding: utf-8 -*-
+
+# YOUR FULL NAME
+# UAG00098
+# Problem Set 3 - Problem 4
+# Description:
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+O arquivo de entrada contém vários números inteiros. O último número no arquivo de entrada é 0.
+Exemplo:
+Digite o inteiro (0 - sair): 5
+Digite o inteiro (0 - sair): 10
+Digite o inteiro (0 - sair): 3
+Digite o inteiro (0 - sair): 0
+
+
+Processes:
+Este programa deve ler uma variável inteira X inúmeras vezes (deve parar quando o valor no arquivo de entrada for igual a zero). Para cada valor lido imprima a sequência de 1 até X, com um espaço entre cada número e seu sucessor.
+
+Output(s):
+Para cada número N do arquivo de entrada deve ser impressa uma linha de 1 até N, conforme o exemplo abaixo. Não deve haver espaço em branco após o último valor da linha.
+Exemplo:
+A sequência é: 1 2 3 4 5
+A sequência é: 1 2 3 4 5 6 7 8 9 10
+A sequência é: 1 2 3
+"""
+
+
+def main():
+    while True:
+        n = int(input("Digite um valor para X (ou 0 para sair): "))
+        if n == 0:
+            break
+        sequencia = ' '.join(str(i) for i in range(1, n + 1))
+        print(f"A sequência até {n} é {sequencia}")
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
